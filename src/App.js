f21c27d44ce4d20942eb4fa3e9ae2df8c607a632
ferutizi <fer.utizi@gmail.com>
2022-12-26 20:03:54 -0300

    map grid in a table

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
 function App() {
-
+  
   const grid = [
     ['1', '2', '3'],
     ['4', '5', '6'],
     ['7', '6', '9'],
   ]
+
+  //grid positions
+  const p1 = grid[0][0];
+  const p2 = grid[0][1];
+  const p3 = grid[0][2];
+  const p4 = grid[1][0];
+  const p5 = grid[1][1];
+  const p6 = grid[1][2];
+  const p7 = grid[2][0];
+  const p8 = grid[2][1];
+  const p9 = grid[2][2];
   
-  const win = () => {
-    //win cases
-    const r0 = grid[0].join('');
-    const r1 = grid[1].join('');
-    const r2 = grid[2].join('');
-    const c0 = grid[0][0].concat(grid[1][0]).concat(grid[2][0]);
-    const c1 = grid[0][1].concat(grid[1][1]).concat(grid[2][1]);
-    const c2 = grid[0][2].concat(grid[1][2]).concat(grid[2][2]);
-    const d1 = grid[0][0].concat(grid[1][1]).concat(grid[2][2]);
-    const d2 = grid[0][2].concat(grid[1][1]).concat(grid[2][0]);
+  //win cases
+  const r0 = grid[0].join('');
+  const r1 = grid[1].join('');
+  const r2 = grid[2].join('');
+  const c0 = p1.concat(p4).concat(p7);
+  const c1 = p2.concat(p5).concat(p8);
+  const c2 = p3.concat(p6).concat(p9);
+  const d1 = p1.concat(p5).concat(p9);
+  const d2 = p3.concat(p5).concat(p7);
 
-    const winCases = [r0, r1, r2, c0, c1, c2, d1, d2];
+  const winCases = [r0, r1, r2, c0, c1, c2, d1, d2];
 
+  const win = (winCases) => {
     const winPlayer1 = winCases.filter(e => e == '111');
     const winPlayer2 = winCases.filter(e => e == '222');
     
@@ -27,13 +38,25 @@ function App() {
     }
     if(winPlayer2.length > 0) {
       console.log('gano player 2');
-      
     }
+  }
+
+  const move = (indexRow, index) => {
+    console.log(indexRow, index);
   }
 
   return (
     <>
       <h1>Tic-Tac-Toe</h1>
+      <table>
+        <tbody>
+          {grid.map((row, indexRow) => 
+            <tr key={indexRow}>{row.map((item, index) =>
+              <td key={index} onClick={() => move(indexRow, index)}>{item}</td>
+              )}</tr>
+            )}
+        </tbody>
+      </table>
     </>
   );
 }
